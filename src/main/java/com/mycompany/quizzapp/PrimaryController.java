20993bea81dc0a5ba78f748583995f346df7d189
unknown <tungle07102005@gmail.com>
2025-06-17 10:08:35 +0700

    upd

--- src/main/java/com/mycompany/quizzapp/PrimaryController.java
+++ src/main/java/com/mycompany/quizzapp/PrimaryController.java
@@ -4,15 +4,23 @@ import com.mycompany.utils.MyAlert;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
 public class PrimaryController {
-    public void handleQuestionManagment(ActionEvent event){
-        MyAlert.getInstance().showMsg("QuestionManagment : Comming soon...");
+
+    public void handleQuestionManagment(ActionEvent event) throws IOException {
+//        MyAlert.getInstance().showMsg("QuestionManagment : Comming soon...");
+        Scene scene = new Scene(new FXMLLoader(App.class.getResource("questions.fxml")).load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
-    
-    public void handlePractice(ActionEvent event){
+
+    public void handlePractice(ActionEvent event) {
         MyAlert.getInstance().showMsg("Pratice : Comming soon...");
     }
-   
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.quizzapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.quizzapp to javafx.fxml;
     exports com.mycompany.quizzapp;
