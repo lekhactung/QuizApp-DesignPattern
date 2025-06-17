682e72c1dd0413b8908114835643ddd2b6762b67
unknown <tungle07102005@gmail.com>
2025-06-17 09:02:12 +0700

    upd

--- src/main/java/com/mycompany/quizzapp/PrimaryController.java
+++ src/main/java/com/mycompany/quizzapp/PrimaryController.java
@@ -1,12 +1,18 @@
 package com.mycompany.quizzapp;
 
+import com.mycompany.utils.MyAlert;
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
 public class PrimaryController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void handleQuestionManagment(ActionEvent event){
+        MyAlert.getInstance().showMsg("QuestionManagment : Comming soon...");
     }
+    
+    public void handlePractice(ActionEvent event){
+        MyAlert.getInstance().showMsg("Pratice : Comming soon...");
+    }
+   
 }
